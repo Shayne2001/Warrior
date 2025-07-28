32271f2f35c47f29e0236c692e409cbd890353df
2556673281@qq.com <2556673281@qq.com>
2025-07-28 22:17:14 +0800

    Sction 9 finished

---
+++ Warrior/Public/AbilitySystem/Abilities/HeroGameplayAbility_PickUpStones.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/WarriorHeroGameplayAbility.h"
+#include "HeroGameplayAbility_PickUpStones.generated.h"
+
+class AWarriorStoneBase;
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UHeroGameplayAbility_PickUpStones : public UWarriorHeroGameplayAbility
+{
+	GENERATED_BODY()
+	
+protected:
+	//~ Begin UGamplayAbility Interface
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	//~ End UGamplayAbility Interface
+
+	UFUNCTION(BlueprintCallable)
+	void CollectStones();
+
+	UFUNCTION(BlueprintCallable)
+	void ConsumeStones();
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	float BoxTraceDistance = 50.f;
+
+	UPROPERTY(EditDefaultsOnly)
+	FVector TraceBoxSize = FVector(100.f);
+
+	UPROPERTY(EditDefaultsOnly)
+	TArray<TEnumAsByte<	EObjectTypeQuery> > StoneTraceChannel;
+
+	UPROPERTY(EditDefaultsOnly)
+	bool bDrawDebugShape = false;
+
+	UPROPERTY()
+	TArray<AWarriorStoneBase*> CollectedStones;
+};
